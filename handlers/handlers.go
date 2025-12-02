ff8a71fb62d8dae15d872bd4ccb3b646089d3af1
ltnz7 <lautarotintez@proton.me>
2025-12-02 12:40:34 -0300

    Feat: add swagger annotations

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -19,11 +19,30 @@ func NewBooksHandler(booksProvider providers.BooksProvider) *BooksHandler {
 	return &BooksHandler{booksProvider: booksProvider}
 }
 
+// GetBooks godoc
+// @Summary Get all books
+// @Description Get a list of all available books
+// @Tags books
+// @Accept json
+// @Produce json
+// @Success 200 {array} models.Book
+// @Router /books [get]
 func (h *BooksHandler) GetBooks(ctx *gin.Context) {
 	books := h.booksProvider.GetBooks(ctx.Request.Context())
 	ctx.JSON(http.StatusOK, books)
 }
 
+// GetMetrics godoc
+// @Summary Get books metrics
+// @Description Get statistical metrics about books, optionally filtered by author
+// @Tags books
+// @Accept json
+// @Produce json
+// @Param author query string false "Author name to filter metrics"
+// @Success 200 {object} providers.BooksMetrics
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /books/metrics [get]
 func (h *BooksHandler) GetMetrics(ctx *gin.Context) {
 	var query GetMetricsRequest
 	if err := ctx.ShouldBindQuery(&query); err != nil {
