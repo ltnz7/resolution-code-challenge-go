6f356e09dd982bff38e69420a5ee44e37b5f24a6
ltnz7 <lautarotintez@proton.me>
2025-12-02 11:55:01 -0300

    feat: implement BooksProvider

--- providers/books.go
+++ providers/books.go
@@ -2,10 +2,33 @@ package providers
 
 import (
 	"context"
+	"log"
 
 	"educabot.com/bookshop/models"
+	"educabot.com/bookshop/repositories"
 )
 
 type BooksProvider interface {
 	GetBooks(ctx context.Context) []models.Book
+}
+
+type booksProvider struct {
+	repo   repositories.BooksRepository
+	logger *log.Logger
+}
+
+func NewBooksProvider(logger *log.Logger) BooksProvider {
+	return &booksProvider{
+		repo:   repositories.NewHTTPBooksRepository(logger),
+		logger: logger,
+	}
+}
+
+func (p *booksProvider) GetBooks(ctx context.Context) []models.Book {
+	books, err := p.repo.GetBooks(ctx)
+	if err != nil {
+		p.logger.Printf("Error fetching books: %v", err)
+		return []models.Book{}
+	}
+	return books
 }
